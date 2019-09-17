10ba25a90e637fc4b973f7cef928636ff5e53b5f
Yesid <ybejaranocamacho@gmail.com>
2019-09-17 15:06:54 -0500

    Avance escena campesino 1

---
+++ Tesis/Assets/Scripts/land.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class land : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject text = new GameObject();
+        TextMeshPro t = text.AddComponent<TextMeshPro>();
+        System.Random rnd = new System.Random();
+        int number = rnd.Next(1, 15);
+        t.text = number + " PH";
+        t.fontSize = 10;
+
+        t.transform.localEulerAngles += new Vector3(180, 90, 180);
+        t.transform.localPosition += new Vector3( gameObject.transform.position.x , gameObject.transform.position.y - 1f, gameObject.transform.position.z + 9f);
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Object is inside the trigger");
+    }
+}
