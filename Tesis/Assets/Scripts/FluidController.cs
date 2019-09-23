81c54d0a44897fef4f7f84f5cdedf732681a59d3
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2019-09-22 19:13:55 -0500

    funciona el aguita de dios

--- Tesis/Assets/Scripts/FluidController.cs
+++ Tesis/Assets/Scripts/FluidController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Obi;
 
 public class FluidController : MonoBehaviour
 {
 
 	public GameObject flash;
+    public ObiEmitter component;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,16 @@ public class FluidController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Local: " + flash.gameObject.transform.localRotation.x);
+
+        Debug.Log("Euler: " + flash.gameObject.transform.eulerAngles.x);
+        if (flash.gameObject.transform.eulerAngles.x <= 90)
+        {
+            component.lifespan = 5f;
+        }
+        else
+        {
+            component.lifespan = 0f;
+        }
     }
 }
