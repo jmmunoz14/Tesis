fdb3779df38eb60091487559cf44eefc890c543c
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2019-10-24 15:39:39 -0500

    Plant Handler and plant

---
+++ Tesis/Assets/PlantHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantHandler : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		FarmLandController x = collision.gameObject.GetComponent<FarmLandController>();
+		x.plant();
+	}
+}
--- Tesis/Assets/Scripts/FarmLandController.cs
+++ Tesis/Assets/Scripts/FarmLandController.cs
@@ -7,16 +7,31 @@ public class FarmLandController : MonoBehaviour
     public enum plantType {zanahoria, maiz, tomate}
     public plantType plantOption;
     public GameObject plantedPlant;
+	private GameObject child;
+	private int numberOfElements = 6;
     public Material[] materials;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void plant()
+	{
+		Vector3 center = transform.position;
+		for (int i = 0; i < numberOfElements; i++){
+			Vector3 pos = RandomCircle(center,2f);
+			child = Instantiate(plantedPlant, pos, Quaternion.Euler(0,0,0));
+			child.transform.parent = transform;
+		}
+	}
+		
+
+
+	Vector3 RandomCircle (Vector3 center, float radius  ){
+		float angx = Random.Range(-2f,2f);
+		float angy = Random.Range(-2f,2f);
+		Vector3 pos;
+		pos.x = center.x + angx;
+		pos.y = center.y;
+		pos.z = center.z + angy;
+		return pos;
+	}
+
 }
