ccc9ffb5ea0f57080af6b888ea2f6e4173a0bc14
Juan Sebastian Arciniegas <js.arciniegas10@uniandes.edu.co>
2019-11-06 23:09:08 -0500

    This is gonna get Down

---
+++ Tesis/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public GameObject menu;
+	public GameObject credits;
+
+	public void PlayNormalMode()
+	{
+		SceneManager.LoadScene("LandScene");
+	}
+
+	public void PlaySurvivalMode()
+	{
+		SceneManager.LoadScene("LandScene");
+	}
+
+	public void PlayTutorial()
+	{
+		SceneManager.LoadScene("Tutorial");
+	}
+
+	public void OpenCredits()
+	{
+		menu.SetActive(false);
+		credits.SetActive(true);
+	}
+
+	public void CloseCredits()
+	{
+		credits.SetActive(false);
+		menu.SetActive(true);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
